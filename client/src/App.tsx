4fb7035100025e8fb40e695c1928719895253970
moskalyk <morgan.moskalyk@protonmail.ch>
2023-06-22 16:42:56 -0400

    added project files

---
+++ client/src/App.tsx
@@ -0,0 +1,207 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import "chartjs-plugin-streaming";
+import moment from "moment";
+import { io } from 'socket.io-client';
+
+const Chart = require("react-chartjs-2").Chart;
+
+const chartColors = {
+  red: "rgb(255, 99, 132)",
+  orange: "rgb(255, 159, 64)",
+  yellow: "rgb(255, 205, 86)",
+  green: "rgb(75, 192, 192)",
+  blue: "rgb(54, 162, 235)",
+  purple: "rgb(153, 102, 255)",
+  grey: "rgb(201, 203, 207)"
+};
+
+const color = Chart.helpers.color;
+const data = {
+  datasets: [
+    {
+      label: "Dataset 1 (linear interpolation)",
+      backgroundColor: color(chartColors.red)
+        .alpha(0.5)
+        .rgbString(),
+      borderColor: chartColors.red,
+      fill: false,
+      lineTension: 0,
+      borderDash: [8, 4],
+      data: []
+    },
+    {
+      label: "Dataset 2 (linear interpolation)",
+      backgroundColor: color(chartColors.red)
+        .alpha(0.5)
+        .rgbString(),
+      borderColor: chartColors.red,
+      fill: false,
+      lineTension: 0,
+      borderDash: [8, 4],
+      data: []
+    }
+  ]
+};
+
+const options = {
+  elements: {
+    line: {
+      tension: 0.5
+    }
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "realtime",
+        distribution: "linear",
+        realtime: {
+          onRefresh: function(chart: any) {
+            chart.data.datasets[0].data.push({
+              x: moment(),
+              y: 1
+            });
+            chart.data.datasets[1].data.push({
+              x: moment(),
+              y: 1
+            });
+          },
+          delay: 3000,
+          time: {
+            displayFormat: "h:mm"
+          }
+        },
+        ticks: {
+          displayFormats: 1,
+          maxRotation: 0,
+          minRotation: 0,
+          stepSize: 1,
+          maxTicksLimit: 30,
+          minUnit: "second",
+          source: "auto",
+          autoSkip: true,
+          callback: function(value: any) {
+            return moment(value, "HH:mm:ss").format("mm:ss");
+          }
+        }
+      }
+    ]
+  }
+};
+
+let labels: any = []
+
+function App() {
+  const [init, setInit] = React.useState<any>(false)
+  const socket = io('ws://localhost:5000');
+
+  const [chartData, setChartData] = React.useState<any>({
+    labels: [],
+    datasets: [
+      {
+        label: 'Sequence',
+        data: [],
+        fill: false,
+        borderColor: 'black',
+        tension: 0.1,
+      },
+      {
+        label: 'Alchemy',
+        data: [],
+        fill: false,
+        borderColor: 'blue',
+        tension: 0.1,
+      },
+      {
+        label: 'Infura',
+        data: [],
+        fill: false,
+        borderColor: 'orange',
+        tension: 0.1,
+      },
+      {
+        label: 'Quicknode',
+        data: [],
+        fill: false,
+        borderColor: 'cyan',
+        tension: 0.1,
+      },
+      {
+        label: 'Polygon',
+        data: [],
+        fill: false,
+        borderColor: 'purple',
+        tension: 0.1,
+      },
+      {
+        label: 'Ankr',
+        data: [],
+        fill: false,
+        borderColor: 'lightblue',
+        tension: 0.1,
+      },
+      {
+        label: 'Sequence Indexer',
+        data: [],
+        fill: false,
+        borderColor: 'pink',
+        tension: 0.1,
+      }
+    ],
+  });
+
+  React.useEffect(() => {
+    if(!init){
+      socket.on('data', (packet: any) => {
+        labels = [...(labels).slice(-10), new Date().toLocaleTimeString()];
+        console.log(chartData)
+        setChartData({
+          labels: labels,
+          datasets: [
+
+            {
+              ...chartData.datasets[0],
+              data: [...chartData.datasets[0].data.slice(-10), packet.blocks[0]-packet.max],
+            },
+            {
+              ...chartData.datasets[1],
+              data: [...chartData.datasets[1].data.slice(-10), packet.blocks[1]-packet.max],
+            },
+            {
+              ...chartData.datasets[2],
+              data: [...chartData.datasets[2].data.slice(-10), packet.blocks[2]-packet.max],
+            },
+            {
+              ...chartData.datasets[3],
+              data: [...chartData.datasets[3].data.slice(-10), packet.blocks[3]-packet.max],
+            },
+            {
+              ...chartData.datasets[4],
+              data: [...chartData.datasets[4].data.slice(-10), packet.blocks[4]-packet.max],
+            },
+            {
+              ...chartData.datasets[5],
+              data: [...chartData.datasets[5].data.slice(-10), packet.blocks[5]-packet.max],
+            },
+            {
+              ...chartData.datasets[6],
+              data: [...chartData.datasets[6].data.slice(-10), packet.blocks[6]-packet.max],
+            },
+          ],
+        });
+      });
+
+      setInit(true)
+    }
+  }, [chartData]);
+
+  return (
+    <div className="App">
+      <h1 style={{textAlign: 'center'}}>web3 RPC monitor</h1>
+      <br/>
+      <Line data={chartData} />
+    </div>
+  );
+}
+
+export default App;
