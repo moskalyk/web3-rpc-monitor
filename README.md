dd0b37ff4b615a41f06c485b8468ec3fc1047f65
moskalyk <morgan.moskalyk@protonmail.ch>
2023-06-22 16:38:45 -0400

    added project files

---
+++ README.md
@@ -0,0 +1,4 @@
+# web3-rpc-monitor
+Updates every a graph every 2 seconds to show a comparison across 6 RPC endpoints the difference in max block number
+
+![rpc monitor](./rpc_monitor.png)
---
+++ socket/index.ts
@@ -0,0 +1,61 @@
+import * as dotenv from "dotenv";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { ethers } from 'ethers'
+import {fetch} from 'cross-fetch'
+
+dotenv.config();
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:3001",
+      methods: ["GET", "POST"]
+    }
+  });
+
+io.on("connection", (socket) => {
+  // ...
+  console.log('connected')
+});
+
+const provider_urls = [
+    process!.env!.SEQUENCE_RPC!, 
+    process!.env!.ALCHEMY_RPC!, 
+    process!.env!.INFURA_RPC!, 
+    process!.env!.QUICKNODE_RPC!, 
+    process!.env!.POLYGON_RPC!, 
+    process!.env!.ANKR_RPC!
+]
+
+const providers: any = []
+
+provider_urls.map((url: string) => {
+    providers.push(new ethers.providers.JsonRpcProvider(url))
+})
+
+setInterval(async () => {
+    const blocks: any = []
+    let i = 0
+    try {
+        for(; i < providers.length; i++){
+            blocks.push(await providers[i].getBlockNumber())
+        }
+
+        const res = await fetch('https://polygon-indexer.sequence.app/status')
+        const json = (await res.json())
+        console.log(json)
+        blocks.push(json.checks.lastBlockNum)
+
+        io.sockets.emit("data", {
+            date: Date.now(), 
+            blocks: blocks,
+            max: Math.max(...blocks)
+        });
+    }catch(err){
+        console.log(i)
+        console.log(err)
+    }
+}, 2000)
+
+httpServer.listen(5000);
